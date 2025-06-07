26f456b2a3a9af3e703ad8145c0b6911300cb95f
alphaville1 <iabdhdvdusbakhsb@protonmail.com>
2025-06-07 23:07:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Andrew Wijaya")
+st.subheader("Selamat datang di website Andrew Wijaya")
+st.write("SMAN 20 Bandung")
+
+st.image("link", width=300, caption="Persib Juara 2025")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.header("About Andrew Wijaya")
+    st.write("Greetings Visitor☺️ Lemme introduce myself, my name is Andrew Wijaya currently I am studying at SMAN 20 Bandung and sit in class X rn.")
+with col2:
+    st.header("Things I love?")
+    st.write("I love sport like karate and learn about hacking because i love it and it becomes my entertainment compared to playing shit games... uhm.")
