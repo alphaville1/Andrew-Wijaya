e5aead2c39e85ef7c953321216b6e4acf8b9c64e
alphaville1 <iabdhdvdusbakhsb@protonmail.com>
2025-06-08 01:31:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,34 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Andrew Wijaya",
+    page_icon="👦",
+    layout="wide"
+)
+
 st.title("Andrew Wijaya")
+
 st.subheader("Welcome to website Andrew Wijaya")
 st.write("SMAN 20 Bandung")
-
-st.image("https://ibb.co/1G7BShz0", width=200, caption="XD.Lmaoo")
+st.image(
+    "https://i.ibb.co/1G7BShz0/IMG-20250511-WA0012.jpg",
+    width=200,
+    caption="XD.Lmaoo"
+)
 
 col1, col2 = st.columns(2)
 with col1:
     st.header("About Me?")
-    st.write("Greetings Visitor☺️ Lemme introduce myself, my name is Andrew Wijaya currently I am grade 10 studying at SMAN 20 Bandung rn.")
+    st.write(
+        "Greetings Visitor☺️ Lemme introduce myself, my name is Andrew Wijaya. "
+        "Currently I am grade 10 studying at SMAN 20 Bandung rn."
+    )
 with col2:
     st.header("Things I love?")
-    st.write("I love sport like karate and learn about cybersecurity because i love it and becomes my entertainment compared to playing games, yeah maybe i've played it once.. uhm, lol.")
-
-import streamlit as st
+    st.write(
+        "I love sport like karate and learn about cybersecurity because I love it. "
+        "It becomes my entertainment compared to playing games, yeah maybe I've played it once.. uhm, lol."
+    )
 
 audio_url = "https://j.top4top.io/m_3445t8myu1.mp3"
 st.audio(audio_url, format='audio/mp3', start_time=0)
