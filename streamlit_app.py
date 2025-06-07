2ea5a121bba421d846dcedee338cabe0eaa69703
alphaville1 <iabdhdvdusbakhsb@protonmail.com>
2025-06-08 01:58:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(
 st.title("Andrew Wijaya")
 
 st.subheader("Welcome to My Websites")
-st.write("Learn coding with me😏")
+st.write("Learn coding with me🤓")
 st.image(
     "https://1.bp.blogspot.com/-lygs2wtQzgk/Ufk2tlT_cOI/AAAAAAAAAGU/duIUyTw3Ues/s1600/troll_face8.png",
     width=300,
