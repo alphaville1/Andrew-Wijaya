c651b08ee2ea0463246079f1680767d7a8eb20e0
alphaville1 <iabdhdvdusbakhsb@protonmail.com>
2025-06-08 00:18:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ with col1:
     st.write("Greetings Visitor☺️ Lemme introduce myself, my name is Andrew Wijaya currently I am studying at SMAN 20 Bandung and sit in class X rn.")
 with col2:
     st.header("Things I love?")
-    st.write("I love sport like karate and learn about cybersecurity because i love it and it becomes my entertainment compared to playing shit games... uhm.")
+    st.write("I love sport like karate and learn about cybersecurity because i love it and it becomes my entertainment compared to playing shit games... uhm, lol.")
