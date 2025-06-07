7c30152375b9b5a9c412a17d87c0dda8fb3daea2
alphaville1 <iabdhdvdusbakhsb@protonmail.com>
2025-06-08 01:12:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("Andrew Wijaya")
 st.subheader("Welcome to website Andrew Wijaya")
 st.write("SMAN 20 Bandung")
 
-st.image("https://i.ibb.co/0STQbRr/7149e6635fef.jpg", width=300, caption="XD.Lmaoo")
+st.image("https://ibb.co/1G7BShz0", width=300, caption="XD.Lmaoo")
 
 col1, col2 = st.columns(2)
 with col1:
@@ -12,4 +12,9 @@ with col1:
     st.write("Greetings Visitor☺️ Lemme introduce myself, my name is Andrew Wijaya currently I am grade 10 studying at SMAN 20 Bandung rn.")
 with col2:
     st.header("Things I love?")
-    st.write("I love sport like karate and learn about cybersecurity because i love it and it becomes my entertainment compared to playing games, yeah maybe i've played it once.. uhm, lol.")
+    st.write("I love sport like karate and learn about cybersecurity because i love it and becomes my entertainment compared to playing games, yeah maybe i've played it once.. uhm, lol.")
+
+# Add music background
+audio_file = open("https://j.top4top.io/m_3445t8myu1.mp3", "rb")
+audio_bytes = audio_file.read()
+st.audio(audio_bytes, format='audio/mp3', start_time=0)
