f23c1b5ffb7a9ecce966867961dd2cb3bef51ca9
alphaville1 <iabdhdvdusbakhsb@protonmail.com>
2025-06-08 00:23:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.image("https://i.ibb.co/0STQbRr/7149e6635fef.jpg", width=300, caption="XD.Lma
 col1, col2 = st.columns(2)
 with col1:
     st.header("About Andrew Wijaya")
-    st.write("Greetings Visitor☺️ Lemme introduce myself, my name is Andrew Wijaya currently I am studying at SMAN 20 Bandung and sit in class X rn.")
+    st.write("Greetings Visitor☺️ Lemme introduce myself, my name is Andrew Wijaya currently I am studying at SMAN 20 Bandung and sit in grade 10 rn.")
 with col2:
     st.header("Things I love?")
     st.write("I love sport like karate and learn about cybersecurity because i love it and it becomes my entertainment compared to playing shit games... uhm, lol.")
